acquisition function gradients and unit tests. Fixed a bug with weighted extraction.

funbo/acquisition.py
@@ -5,8 +5,11 @@ Acquisition Functions
 
 import numpy as np
 
+# local imports
+from .utils import k_RBF
 
-def UCB(X, beta, surrogate, maximising):
+
+def UCB(X, beta, surrogate, maximising, return_gradient=False):
     """
     Args:
         X: the inputs for the surrogate to predict at (one per row)
@@ -14,11 +17,76 @@ def UCB(X, beta, surrogate, maximising):
         surrogate: the surrogate model to use for predictions
         maximising: whether the optimiser is maximising or minimising (minimising => -LCB rather than UCB)
     """
-    mu, var = surrogate.predict(X)
-    assert mu.shape == var.shape == (X.shape[0], 1)
-    sigma = np.sqrt(np.clip(var, 0, np.inf)) # ensure no negative variance
+    assert len(X.shape) == 2
+
+    # full_cov: whether to return the full covariance matrix or just the diagonal
+    mu, var = surrogate.predict(X, full_cov=False)
+    assert mu.shape == var.shape == (X.shape[0], 1) # (input_rows, outputs)
+
+    sigma = np.sqrt(np.clip(var, 1e-10, np.inf)) # ensure no negative variance
+
     sf = 1 if maximising else -1 # scale factor
     # in this form it is clearer that the value is the negative LCB when minimising
-    # sf * (mus + sf * beta * sigmas)
-    return sf * mu + beta * sigma
+    # sf * (mu + sf * beta * sigma)
+    acq = sf * mu + beta * sigma
+
+    if not return_gradient:
+        return acq
+    else:
+        dmu_dx, dvar_dx = surrogate.predictive_gradients(X)
+        assert dmu_dx.shape == (X.shape[0], X.shape[1], 1) # (input_rows, input_cols, outputs)
+        assert dvar_dx.shape == X.shape # all outputs have the same variance
+
+        dmu_dx = dmu_dx[:, :, 0] # take only the first output
+        assert dmu_dx.shape == X.shape
+
+        # sigma must be strictly > 0 for this, which has been ensured earlier
+        #
+        # $\frac{d\sigma}{dx} = \frac{d\sigma}{d\sigma^2} \frac{d\sigma^2}{dx}$
+        # $=\frac{1}{\frac{d\sigma^2}{d\sigma}} \frac{d\sigma^2}{dx}$
+        #
+        # uses the 'inverse function theorem'
+        # https://math.stackexchange.com/q/185004
+        # https://math.stackexchange.com/q/292590
+        #
+        # by not totally rigorous proof using the chain rule:
+        # $\frac{dx}{dy}\frac{dy}{dx}=\frac{dx}{dx}=1 \quad\implies\quad \frac{dx}{dy}=\frac{1}{\frac{dy}{dx}}$
+        # requires that dx_dy and dy_dx are > 0
+        dsig_dx = dvar_dx / (2*sigma)
+
+        dacq_dx = sf * dmu_dx + beta * dsig_dx
+
+        return acq, dacq_dx
+
+
+
+
+def RBF_weighted(X, Y, dY_dx, center, sigma, l):
+    """ weight the Y values based on the distance of the corresponding X values to the center of an RBF function with length scale l
+
+    Args:
+        X: the input points (as rows) for the Y values to weight
+        Y: the output values to weight
+        dY_dx: the derivative of Y w.r.t X. None => don't calculate weighted gradient.
+        center: the center of the RBF to compare with each point in X
+        l: the length scale of the RBF
+
+    Returns:
+        `weighted_Y if dY_dx is None else (weighted_Y, dweightedY_dx)`
+    """
+    assert Y.shape == (X.shape[0], 1)
+    assert (dY_dx is None) or (dY_dx.shape == X.shape)
+    assert center.shape == (1, X.shape[1])
+    assert np.isscalar(sigma) and np.isscalar(l)
+    # for the input points X, calculate the weight using an RBF function
+    return_gradient = bool(dY_dx is not None)
+    res = k_RBF(X, center, sigma=sigma, l=l, return_gradient=return_gradient)
+    w, dw_dx = res if return_gradient else (res, None)
+    weighted_Y = Y * w
+    if return_gradient:
+        # product rule
+        dweightedY_dx = Y * dw_dx + dY_dx * w
+        return weighted_Y, dweightedY_dx
+    else:
+        return weighted_Y
 

funbo/aux_optimisers.py
@@ -8,10 +8,10 @@ import scipy.optimize
 import warnings
 
 # local imports
-from .utils import uniform_random_in_bounds
+from .utils import uniform_random_in_bounds, show_warnings
 
 
-def maximise_random_quasi_Newton(f, bounds, num_random, num_take_random, num_bfgs, exact_gradient):
+def maximise_random_quasi_Newton(f, bounds, num_random, num_take_random, num_bfgs, exact_gradient, quiet=True):
     """ maximise the given function by first sampling randomly, then taking the
         `num_take_random` best samples and using them as starting points in BFGS
         optimisation.
@@ -25,8 +25,10 @@ def maximise_random_quasi_Newton(f, bounds, num_random, num_take_random, num_bfg
         num_take_random: the number of best random samples to use as starting
             points for BFGS
         num_bfgs: the number of BFGS iterations to perform
-        exact_gradient: whether `f` returns the gradient as well as the values.
-            If False then the gradient is approximated numerically.
+        exact_gradient: whether `f` returns the gradient as well as the values
+            when passed `return_gradient=True`.  If False then the gradient is
+            approximated numerically.
+        quiet: whether to show warnings caused by failed optimisation iterations
 
     Returns:
         `(best_x, best_y)`
@@ -37,7 +39,10 @@ def maximise_random_quasi_Newton(f, bounds, num_random, num_take_random, num_bfg
         r_best_xs, r_best_ys = np.empty(shape=(0, len(bounds))), np.empty(shape=(0, 1))
 
     # can return None, None if every iteration fails
-    g_best_x, g_best_y = maximise_quasi_Newton(f, bounds, num_bfgs, 1, exact_gradient, starting_points=r_best_xs)
+    with warnings.catch_warnings(record=True) as ws:
+        g_best_x, g_best_y = maximise_quasi_Newton(f, bounds, num_bfgs, 1, exact_gradient, starting_points=r_best_xs)
+    if not quiet:
+        show_warnings(ws)
 
     if num_random > 0 and (g_best_y is None or r_best_ys[0] > g_best_y):
         return r_best_xs[0], r_best_ys[0]
@@ -100,7 +105,16 @@ def maximise_quasi_Newton(f, bounds, num_its, num_take, exact_gradient, starting
     neg_ys = []
 
     # the minimiser passes x as (num_attribs,) but f takes (1,num_attribs)
-    opt_fun = lambda x: -f(x.reshape(1, -1))
+    if exact_gradient:
+        # negate both the value and the gradient
+        opt_fun = lambda x: tuple(-v for v in f(x.reshape(1, -1), return_gradient=True))
+    else:
+        opt_fun = lambda x: -f(x.reshape(1, -1))
+
+    #import matplotlib.pyplot as plt
+    #ys = np.linspace(*bounds[0], num=100)
+    #plt.plot(ys, np.array([opt_fun(y) for y in ys]).flatten())
+    #plt.show()
 
     for i in range(num_its):
         result = scipy.optimize.minimize(

funbo/optimiser.py
@@ -8,7 +8,8 @@ from collections import namedtuple
 import warnings
 
 # local imports
-from .utils import FixedAttributes, PiecewiseFunction, k_RBF
+from .utils import FixedAttributes, PiecewiseFunction, k_RBF, show_warnings
+from .acquisition import RBF_weighted
 from .coordinator import *
 
 
@@ -35,8 +36,7 @@ class Trial(FixedAttributes):
 
 
 class Optimiser(FixedAttributes):
-    """ Continuous Function Bayesian Optimisation
-    """
+    """ Continuous Function Bayesian Optimisation """
     slots = (
         'objective', 'domain_names', 'domain_bounds', 'range_bounds',
         'coordinator', 'desired_extremum', 'clip_range', 'has_run', 'trials'
@@ -129,25 +129,40 @@ class Optimiser(FixedAttributes):
         prev_xy = None
         for x in c.control_xs:
 
-            def acq(ys):
+            def get_X(ys):
+                xs = np.repeat(x.reshape(1, -1), ys.shape[0], axis=0) # stack copies of x as rows
+                X = np.hstack((xs, ys))
+                return X
+
+            X = get_X(np.linspace(*self.range_bounds, num=500).reshape(-1, 1))
+            worst_acq = np.min(c.acquisition(X, surrogate=surrogate,
+                                             maximising=self.is_maximising(),
+                                             **c.acquisition_params))
+
+            def acq(ys, return_gradient=False):
                 """ with x fixed, given ys as rows calculate the weighted
                 acquisition function for the pairs of x and y
                 """
-                xs = np.repeat(x.reshape(1, -1), ys.shape[0], axis=0) # stack copies of x as rows
-                X = np.hstack((xs, ys))
-                As = c.acquisition(X, surrogate=surrogate, maximising=self.is_maximising(), **c.acquisition_params)
-                As -= np.min(As) # shift to make 0 the worst possible value, so that at w=0, A*w is the worst possible value.
-                if prev_xy is None:
-                    return As
-                else:
-                    # calculate the tracking weights
-                    # for the input points X, calculate the tracking weight using an RBF function centered at prev_xy
-                    assert prev_xy.shape == (1, X.shape[1])
-                    rs = X - prev_xy # subtract from every row
-                    rs = np.linalg.norm(rs, axis=1) # calculate the norm of every row
-                    ws = k_RBF(rs.reshape(-1, 1), sigma=1, l=c.tracking_l)
+                X = get_X(ys)
+                res = c.acquisition(X, surrogate=surrogate,
+                                   maximising=self.is_maximising(),
+                                   return_gradient=return_gradient,
+                                   **c.acquisition_params)
+                acq, dacq_dx = res if return_gradient else (res, None)
+                # shift to make 0 the worst possible value, so that at w=0,
+                # A*w is the worst possible value.
+                # can't subtract min(acq) because ys may not be many points over the whole domain
+                acq -= worst_acq
 
-                    return As * ws
+                if prev_xy is not None: # can only weight after the first sample
+                    res = RBF_weighted(X, acq, dacq_dx, center=prev_xy, sigma=1, l=c.tracking_l)
+                    acq, dacq_dx = res if return_gradient else (res, None)
+
+                if return_gradient:
+                    dacq_dy = dacq_dx[0, -1:] # take only the derivative w.r.t y as a single-element array
+                    return acq, dacq_dy
+                else:
+                    return acq
 
             best_y, best_acq = c.aux_optimiser(acq, [self.range_bounds], **c.aux_optimiser_params)
             ys.append(np.asscalar(best_y))

funbo/plotting/utils.py
@@ -8,6 +8,8 @@ import numpy as np
 import matplotlib as mpl
 import scipy.interpolate
 
+from funbo.utils import show_warnings
+
 
 def in_jupyter():
     """ whether the current script is running in IPython/Jupyter """
@@ -37,9 +39,6 @@ def multidimensional_integrate(f, domain, quiet=True):
     return result
 
 
-def show_warnings(ws):
-    for w in ws:
-        warnings.showwarning(w.message, w.category, w.filename, w.lineno)
 
 def unzip(l):
     if l:

funbo/utils.py
@@ -3,6 +3,7 @@
 Utility functions and data structures
 """
 
+import warnings
 import numpy as np
 import scipy.interpolate
 
@@ -87,13 +88,31 @@ def uniform_random_in_bounds(num_samples, bounds):
         cols.append(np.random.uniform(rmin, rmax, size=(num_samples, 1)))
     return np.hstack(cols)
 
-def k_RBF(r, sigma, l):
-    """ calculate the RBF values of r with the given standard deviation of sigma and lengthscale of l
+def k_RBF(X, center, sigma, l, return_gradient=False):
+    """ calculate the RBF values of X with a given center, standard deviation and lengthscale
 
     Args:
-        r: either a scalar length or a column of lengths
+        X: the points to evaluate at (one per row)
+        center: the center of the RBF function
+        sigma: the standard deviation of the RBF function
+        l: the lengthscale of the RBF function
+        return_gradient: whether to calculate and return drbf_dx
     """
     assert np.isscalar(sigma) and np.isscalar(l)
-    assert np.isscalar(r) or r.shape[1] == 1 # either a scalar or a column of values
-    return sigma**2 * np.exp(-0.5 * np.square(r/l))
+    assert len(X.shape) == 2
+    assert center.shape == (1, X.shape[1])
+    diff = X - center # subtract from every row
+    rs = np.linalg.norm(diff, axis=1).reshape(-1, 1) # calculate the norm of every row
+    rbf = sigma**2 * np.exp(-0.5 * np.square(rs/l))
+    if return_gradient:
+        # take sigma^2 out
+        # d/dx e^x = e^x
+        # chain rule so multiply by d/dx -1/(2l^2) * (x-center)^2  =  -1/l^2 * (x-center)
+        drbf_dx = -1/(l**2) * diff * rbf
+        return rbf, drbf_dx
+    else:
+        return rbf
 
+def show_warnings(ws):
+    for w in ws:
+        warnings.showwarning(w.message, w.category, w.filename, w.lineno)

tests/test_utils.py
@@ -12,24 +12,39 @@ class TestUtils(unittest.TestCase):
     def test_rbf(self):
         """ test k_RBF against the GPy RBF implementation """
 
-        def k_with_GPy(r, sigma, l):
+        def k_with_GPy(X, center, sigma, l, return_gradient=False):
             with warnings.catch_warnings():
                 warnings.filterwarnings('ignore', '.*')
                 k = GPy.kern.RBF(input_dim=1, variance=sigma**2, lengthscale=l)
-                v = k.K(np.array([[0]]), np.array([[r]]))
-                return np.asscalar(v)
+                v = k.K(X, center)
+                if return_gradient:
+                    dv_dr = k.dK_dr_via_X(X, center)
+                    # remember in GPy, r is the _scaled_ r
+                    # r = |x-center|/l
+                    # d/dx |x|=x/|x|
+                    diff = X - center
+                    dr_dx = diff/(np.abs(diff)*l)
+                    dv_dx = dv_dr * dr_dx
+                    return v, dv_dx
+                else:
+                    return v
 
         sigma, l = 4, 0.5
+        center = np.array([[-1.23]])
 
         # test a single r
-        r = 0.8
-        self.assertTrue(np.isclose(utils.k_RBF(r, sigma, l), k_with_GPy(r, sigma, l)))
+        X = np.array([[0.8]])
+        self.assertTrue(np.isclose(utils.k_RBF(X, center, sigma, l), k_with_GPy(X, center, sigma, l)))
 
         # test rs as a column
-        rs = np.linspace(0.4, 2.5, num=10).reshape(-1, 1)
-        vals = utils.k_RBF(rs, sigma, l)
-        for i, v in enumerate(vals):
-            self.assertTrue(np.isclose(v, k_with_GPy(np.asscalar(rs[i]), sigma, l)))
+        X = np.linspace(-4, 2.5, num=10).reshape(-1, 1)
+        vals = utils.k_RBF(X, center, sigma, l)
+        self.assertTrue(np.allclose(vals, k_with_GPy(X, center, sigma, l)))
+
+        vals, dvals = utils.k_RBF(X, center, sigma, l, True)
+        gpyvals, dgpyvals = k_with_GPy(X, center, sigma, l, True)
+        self.assertTrue(np.allclose(vals, gpyvals))
+        self.assertTrue(np.allclose(dvals, dgpyvals))
 
     def test_uniform_in_bounds(self):
         np.random.seed(0)